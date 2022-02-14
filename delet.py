d3a923890597f447954810fce91f5bcb1e85e2a9
Sergey-2021 <tatarinov.serzhick@yandex.ru>
2022-02-15 00:38:56 +0300

    модуль удаления данных по id

---
+++ delet.py
@@ -0,0 +1,22 @@
+import overwrite
+
+
+def edit_file():
+
+    with open('file.txt', 'r', encoding='utf-8') as data:
+        c: str = ''
+        for i in data:
+            c += i
+        lst = c.split('\n')
+    return lst
+
+
+def del_el(id):
+    r = edit_file()
+    for i in range(len(r)):
+        if id == i:
+            r.pop(i)
+        r[i-1] += '\n'
+    overwrite.overwriting_file(''.join(r))
+
+
